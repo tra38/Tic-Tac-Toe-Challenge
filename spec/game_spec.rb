1c554ab46ff8a078134c13b622a516b65e01168a
tra38 <tra38@nau.edu>
2015-09-12 14:45:28 -0500

    Add test for starting board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,11 @@
 require_relative '../lib/game.rb'
 
-RSpec.describe Game, "#score" do
-	context "It does nothing"
-	it "does nothing" do
-		expect(1).to eq 80
+RSpec.describe Game do
+  before(:all) {
+    @board = Board.new
+    @game = Game.new(@board)
+  }
+	it "starts with a blank board" do
+		expect(@game.board.to_s).to eq ("|_0_|_1_|_2_|\n|_3_|_4_|_5_|\n|_6_|_7_|_8_|\n")
 	end
 end
