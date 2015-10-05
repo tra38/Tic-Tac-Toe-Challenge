23ac47c568b65650fa43bf67d6abc3cccde22649
tra38 <tra38@nau.edu>
2015-10-05 11:48:54 -0500

    Move logic for printing to the console to the View module

--- lib/view.rb
+++ lib/view.rb
@@ -1,11 +1,11 @@
 module View
 
 	def self.welcome
-		"Welcome to my Tic Tac Toe game"
+		puts "Welcome to my Tic Tac Toe game"
 	end
 
 	def self.choose_type_of_match
-		<<-eos
+		puts <<-eos
 		Choose what type of match you want to play:
 
 		1 - Human versus Human
@@ -18,7 +18,7 @@ module View
 		player_one = options_hash[:player_one]
 		player_two = options_hash[:player_two]
 
-		<<-eos
+		puts <<-eos
 
 		1 - Let's play!
 		2 - Allow #{player_two.symbol} to go first!
@@ -30,45 +30,49 @@ module View
 	def self.current_options(options_hash)
 		player_one = options_hash[:player_one]
 		player_two = options_hash[:player_two]
-		<<-eos "This will be a #{player_one.symbol} (#{player_one.type}) versus a #{player_two.symbol} (#{player_two.type}) match. #{player_one.symbol} will go first."
+		puts <<-eos "This will be a #{player_one.symbol} (#{player_one.type}) versus a #{player_two.symbol} (#{player_two.type}) match. #{player_one.symbol} will go first."
 		eos
 	end
 
 	def self.change_symbol(symbol)
-		"Type in the new symbol to replace #{symbol}"
+		puts "Type in the new symbol to replace #{symbol}"
 	end
 
 	def self.invalid_option_selection(input)
-		"I am sorry but #{input} is an invalid selection. Please try again."
+		puts "I am sorry but #{input} is an invalid selection. Please try again."
 	end
 
 	def self.instructions
-		"Please select your spot. Only the spaces between 1-9"
+		puts "Please select your spot. Only the spaces between 1-9"
 	end
 
 	def self.game_over
-		"Game over"
+		puts "Game over"
 	end
 
 	def self.display_turn(symbol)
-		"It is now #{symbol}'s Turn."
+		puts "It is now #{symbol}'s Turn."
 	end
 
 	def self.spot_picked(symbol, spot)
-		"#{symbol} has picked Spot #{spot}."
+		puts "#{symbol} has picked Spot #{spot}."
 	end
 
 	def self.display_error(spot)
-		"I am sorry, but Spot #{spot} is an invalid choice to make. Please make another selection."
+		puts "I am sorry, but Spot #{spot} is an invalid choice to make. Please make another selection."
 	end
 
 	#clear the screen, only works on ANSI-supported screens (source: http://stackoverflow.com/a/263650)
 	def self.clear
-		"\e[H\e[2J"
+		puts "\e[H\e[2J"
 	end
 
 	def self.display_thinking(symbol)
-		"#{symbol} is thinking..."
+		puts "#{symbol} is thinking..."
+	end
+
+	def self.display_board(board)
+		puts board
 	end
 
 end
